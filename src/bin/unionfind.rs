7f3c60aa48d7285097c816085e8f688d2ed794e3
chiaoicchi <182973681+chiaoicchi@users.noreply.github.com>
2026-01-18 16:20:51 +0900

    impl disjoint set

---
+++ src/bin/unionfind.rs
@@ -0,0 +1,24 @@
+use data_strux::disjoint_set::Dsu;
+use std::io::{BufWriter, Write, read_to_string, stdin, stdout};
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut stdin = stdin.split_whitespace();
+    let mut stdout = BufWriter::new(stdout().lock());
+
+    let n: usize = stdin.next().unwrap().parse().unwrap();
+    let q: usize = stdin.next().unwrap().parse().unwrap();
+
+    let mut dsu = Dsu::new(n);
+
+    for _ in 0..q {
+        let t: u8 = stdin.next().unwrap().parse().unwrap();
+        let u: usize = stdin.next().unwrap().parse().unwrap();
+        let v: usize = stdin.next().unwrap().parse().unwrap();
+        if t == 0 {
+            dsu.unite(u, v);
+        } else {
+            writeln!(stdout, "{}", dsu.same(u, v) as u8).ok();
+        }
+    }
+}
