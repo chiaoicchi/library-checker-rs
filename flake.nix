bc5e48f9c4b6df9e09009c5df3d655d9f510b84a
chiaoicchi <182973681+chiaoicchi@users.noreply.github.com>
2026-01-18 16:19:34 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Library Checker for Rust environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        rustToolchain =
+          (fenix.packages.${system}.toolchainOf {
+            channel = "1.89.0";
+            sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+          }).withComponents
+            [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rust-analyzer"
+              "rustc"
+              "rustfmt"
+            ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+          ];
+
+          RUST_BACKTRACE = "1";
+
+          shellHook = ''
+            echo "Library Checker for Rust Environment (fenix)"
+            echo "  Rust $(rustc --version)"
+          '';
+        };
+      }
+    );
+}
